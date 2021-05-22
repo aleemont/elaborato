cc9d348681dd709944cdde666f849a57d1934b51
Alessandro Monticelli <alexmonticelli02@gmail.com>
2021-05-22 23:23:12 +0200

    Major Update: Sito dinamico

--- index.php
+++ index.php
@@ -4,188 +4,222 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/main.css">
     <!--Bootstrap CSS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/colors.css">
+    <link rel="stylesheet" href="./assets/css/main.css">
     <title>eShop</title>
 </head>
 <body>
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-default bg-primary">
         <div class="navbar-brand">
             <a href="#" class="h3">eShop</a>
         </div>
     </nav>
+    <!--Inizializzo la connessione al DB -->
+    <?php
+      $user = "root";
+      $pass = "Ale-26062002";
+      try {
+        $dbh = new PDO('mysql:host=localhost;dbname=elaborato', $user, $pass);
+      } catch (PDOException $e) {
+        print "Errore nella connessione al DataBase!: " . $e->getMessage() . "<br/>";
+        die();
+      }
+    ?>
     <div class="container">
       <div class="row mt-3 mb-4">
     <!--Left wrapper start-->
+    
       <div class="container d-none d-lg-block col-lg-3">
         <div class="card rounded">
-          <div class="card-header">
-            <h2>Filtri di ricerca</h2>
+          <div class="card-header bg-secondary">
+            <h2 class="text-white">Filtri di ricerca</h2>
           </div>
           <div class="card-body">
             <span>Tipo di dispositivo:</span>
             <ul>
-              <li><input type="checkbox"/> Portatili</li> <!--$prod_type = fetch(Select * from schema_name.table_name)-->
-              <li><input type="checkbox"/> Smartphone</li>
-              <li><input type="checkbox"/> Tablet</li>
-              <li><input type="checkbox"/> Wearable</li>
+              <?php
+                $stm = $dbh->query("SELECT DISTINCT Categoria FROM Prodotto");
+                $categorie = $stm->fetchAll();
+              ?>
+              <?php 
+                foreach($categorie as $categoria){
+              ?>
+              <li><input type="checkbox"/> <?php 
+                switch($categoria["Categoria"]){
+                  case 0:
+                    echo "Laptop";
+                    break;
+                  case 1:
+                    echo "Smartphone";
+                    break;
+                  case 2:
+                    echo "Tablet";
+                    break;
+                  default:
+                    break;
+                }
+              ?></li>
+              <?php
+                }  
+                $stm = null;
+              ?>
             </ul>
             <p>Seleziona Caratteristiche:</p>
             <ul>
               <li>Ram
                 <ul>
+                <?php
+                    $stm = $dbh->query("SELECT DISTINCT Dimensione FROM RAM ORDER BY Dimensione ASC");
+                    $RAMs = $stm->fetchAll();
+                    foreach($RAMs as $RAM){
+                ?>
                   <li>
-                    <input type="checkbox"/> 4GB  <!--$RAM_array = fetch(Select size from RAM) -->
+                    <input type="checkbox" />
+                    <?php echo $RAM["Dimensione"];?> GB
                   </li>
-                  <li>
-                    <input type="checkbox"/> 8GB
-                  </li>
-                  <li>
-                    <input type="checkbox"/> 16GB
-                  </li>
-                  <li>
-                    <input type="checkbox"/> 32GB
-                  </li>
-                  <li>
-                    <input type="checkbox"/> 64GB
-                  </li>
+                <?php      
+                  }
+                  $stm = null;
+                ?>
                 </ul>
               </li>
+              <br />
               <li>Cpu
                 <ul>
-                  <li>
-                    <input type="checkbox"/> Intel
-                  </li>
-                    <ul>
-                      <li>
-                        <input type="checkbox"/> Core i3  <!--$CPU_array = fetch(Select * from CPU where cpu_brand='intel') -->
-                      </li>
-                      <li>
-                        <input type="checkbox"/> Core i5
-                      </li>
-                      <li>
-                        <input type="checkbox"/> Core i7
-                      </li>
-                      <li>
-                        <input type="checkbox"/> Core i9
-                      </li>
-                    </ul>
-                    <li>
-                      <input type="checkbox"/> AMD
-                    </li>
-                      <ul>
-                        <li>
-                          <input type="checkbox"/> Ryzen 3  <!--$CPU_array = fetch(Select + from CPU where cpu_brand='amd') -->
-                        </li>
-                        <li>
-                          <input type="checkbox"/> Ryzen 5
-                        </li>
-                        <li>
-                          <input type="checkbox"/> Ryzen 7
-                        </li>
-                        <li>
-                          <input type="checkbox"/> Ryzen 9
-                        </li>
-                      </ul>
+                  <?php
+                      $stm = $dbh->query("SELECT DISTINCT Produttore FROM CPU ORDER BY Produttore ASC");
+                      $produttori = $stm->fetchAll();
+                  ?>
+                        <?php 
+                          foreach($produttori as $produttore){
+                            ?>
+                            <li><input type="checkbox"/> <?php echo $produttore["Produttore"]; ?></li>
+                        <?php
+                          }
+                          $stm = null;
+                    ?>
                 </ul>
               </li>
+              <br />
               <li>
                 Disco
                 <ul>
-                  <li><input type="checkbox"/> SSD</li> <!--Select + from DISK -->
-                  <li><input type="checkbox"/> HDD</li>
+                <?php
+                      $stm = $dbh->query("SELECT DISTINCT Tipo FROM Disco");
+                      $dischi = $stm->fetchAll();
+                      $stm = null;
+                  ?>
+                        <?php 
+                          foreach($dischi as $disco){
+                            ?>
+                            <li><input type="checkbox"/> <?php echo $disco["Tipo"]; ?></li>
+                        <?php
+                          }  
+                          $stm = null;
+                    ?>
                 </ul>
               </li>
             </ul>
           </div>
-          <div class="card-footer"><input type="submit" name="filter" value="Filtra" class="btn btn-secondary w-100"></div>
+          <div class="card-footer bg-white d-flex"><input type="submit" name="filter" value="Filtra" class="btn btn-secondary w-100"></div>
         </div>
       </div>
     <!--Left wrapper end-->
     <!--Main wrapper start-->
       <div class="d-block col-md-12 col-lg-9">
         <div class="card rounded">
-          <div class="card-header">
-            <h2>Prodotti in offerta</h2>
+          <div class="card-header bg-secondary">
+            <h2 class="text-white">Prodotti in offerta</h2>
           </div>
-          <div class="card-body row">
-              <div class="col-md-6">  
+          <div class="card-body row p-1 p-md-5">
+              <div class="col-12">
+                <div class="row">
+                  <?php
+                    $stm = $dbh->query("SELECT * FROM Prodotto");
+                    $prodotti = $stm->fetchAll(PDO::FETCH_ASSOC);
+                    $stm = null;
+                    if(!$prodotti) exit("Nessun prodotto disponibile");
+                    foreach($prodotti as $prodotto){
+                  ?>
+                  <div class="col-lg-6 col-12">
+                  <div class="card rounded mr-3 mb-3 w-100">
+                    <div class="card-header">IMG</div> 
+                    <div class="card-body">
+                      <?php echo $prodotto["Nome"]."&nbsp;"?>
+                    </div>
+                    <div class="accordion" id="accordion" role="tablist">
+                      <div class="card">
+                        <div class="card-header bg-secondary" role="tab" id="heading-<?php echo $prodotto["ID"]; ?>">
+                          <h6 class="mb-0"> <a data-toggle="collapse" href="#collapse-<?php echo $prodotto["ID"];?>" aria-expanded="false" aria-controls="collapse-<?php echo $prodotto["ID"];?>" data-abc="true" class="collapsed text-white"> Specifiche Tecniche </a> </h6>
+                        </div>
+                        <div id="collapse-<?php echo $prodotto["ID"];?>" class="collapse bg-success" role="tabpanel" aria-labelledby="heading-<?php echo $prodotto["ID"];?>" data-parent="#accordion" style="">
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-12">
+                              <?php 
+                                $stm = $dbh->prepare("SELECT Produttore,Serie,Modello,Cores,Threads,Frequenza FROM CPU WHERE ID = ?");
+                                $stm->execute([$prodotto["CPU"]]);
+                                $CPU = $stm->fetch(PDO::FETCH_ASSOC);
+                                $stm = null;
+                              ?>
+                              <span class="text-secondary font-weight-bold">CPU:</span>
+                              <div class="list-group p-0">
+                                  <div class="list-group-item"><?php echo $CPU["Produttore"]." ".$CPU["Serie"]." ".$CPU["Modello"];?></div>
+                                  <div class="list-group-item"><?php echo $CPU["Cores"]."C/".$CPU["Threads"]."T@".$CPU["Frequenza"]."GHz";?></div>
+                              </div>
+
+                              <?php 
+                                $stm = $dbh->prepare("SELECT Dimensione,Tipo FROM RAM WHERE ID = ?");
+                                $stm->execute([$prodotto["RAM"]]);
+                                $RAM = $stm->fetch(PDO::FETCH_ASSOC);
+                                $stm = null;
+                              ?>
+                              <span class="text-secondary font-weight-bold">RAM:</span>
+                              <div class="list-group p-0">
+                                  <div class="list-group-item"><?php echo $RAM["Dimensione"]." ".$RAM["Tipo"];?></div>
+                              </div>
+
+                              <?php 
+                                $stm = $dbh->prepare("SELECT Dimensione,Tipo,Modello,Velocità  FROM Disco WHERE ID = ?");
+                                $stm->execute([$prodotto["Disco"]]);
+                                $Disco = $stm->fetch(PDO::FETCH_ASSOC);
+                                $stm = null;
+                              ?>
+                              <span class="text-secondary font-weight-bold">Disco:</span>
+                              <div class="list-group p-0">
+                                  <div class="list-group-item"><?php echo $Disco["Tipo"]." ".$Disco["Dimensione"]."GB"." ".$Disco["Modello"];
+                            ?></div>
+                            
+                            <?php 
+                                $stm = $dbh->prepare("SELECT Diagonale,Tipo,Risoluzione FROM Schermo WHERE ID = ?");
+                                $stm->execute([$prodotto["Schermo"]]);
+                                $Schermo = $stm->fetch(PDO::FETCH_ASSOC);
+                                $stm = null;
+                              ?>
+                              <span class="text-secondary font-weight-bold">Display:</span>
+                              <div class="list-group p-0">
+                                  <div class="list-group-item"><?php echo $Schermo["Diagonale"]."\" ".$Schermo["Tipo"]." ".$Schermo["Risoluzione"];?>
+                              </div>                      
+                            </div>
+                            </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    <div class="m-3 p-0 pl-3"> 
+                      <button type="button w-50" class="btn btn-primary font-weight-semibold" >Acquista ora</button>
+                      <button type="button w-50" class="btn btn-secondary font-weight-bold" style="background-color:orange !important;">Aggiungi al carrello</button>
+                    </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
                 <?php
-                  for($i=0; $i<5; $i++){ // $prods = Select COUNT(prod_name) from products / 2; $first_col = $prods/2 for($i=0; $i<$first_col; $i++)
-                    echo '
-                      <div class="card rounded mt-2 mb-2">
-                        <div class="card-header">
-                          <img class="img-fluid"src="assets/img/prod.jpg" alt="Laptop"/>
-                        </div>
-                        <div class="card-body">
-                          <h6>Laptop 15" Intel Core i3 8Gb RAM 512GB SSD</h6>  <!--SELECT prod_name FROM products WHERE id=\'$i\'-->
-                          <h6 class="list-group p-2">
-                            <div class="list-group-item">
-                                <span>CPU</span>:                                                       <!--$cpu_id = SELECT cpu FROM products WHERE id=\'$i\'-->
-                                <ul>              
-                                    <li><span>Intel</span> <span>Core i3</span> <span>11300</span></li> <!--SELECT cpu_brand, cpu_fam, cpu_model FROM CPU WHERE id=\'$cpu_id\'-->
-                                    <li><span>4</span> core, <span>4</span> thread @<span>2.2</span>GHz</li> <!--SELECT cpu_cores, cpu_threads, cpu_speed FROM CPU WHERE id=\'$cpu_id\'-->
-                                </ul>
-                            </div>
-                            <div class="list-group-item">
-                                <span>RAM</span> 
-                                <ul>
-                                    <li>Quantità: <span>8</span>GB</li>
-                                    <li>Tipo:<span>DDR4</span> @<span>2666</span>MHz</li>
-                                </ul>
-                            </div>
-                            <div class="list-group-item">
-                                <span>Disco</span>: 
-                                <ul>
-                                    <li><span>SSD</span> <span>SATA</span></li>
-                                    <li>Dimensione: <span>512</span>GB</li>
-                                </ul>
-                            </div>
-                        </h6>
-                        </div>
-                        </div>';
                   }
                 ?>
               </div>
-              <div class="col-md-6">
-              <?php
-                  for($i=0; $i<5; $i++){  // for($i=$first_col; $i<$prods; $i++)
-                    echo '
-                      <div class="card rounded mt-2 mb-2">
-                        <div class="card-header">
-                          <img class="img-fluid"src="assets/img/matebook.jpg" alt="Laptop"/>
-                        </div>
-                        <div class="card-body">
-                          <h6>Laptop 13" AMD Ryzen 5 4600H 16Gb RAM 512GB SSD</h6> <!--SELECT prod_name FROM products WHERE id=\'$i\'-->
-                          <h6 class="list-group p-2">
-                            <div class="list-group-item">
-                                <span>CPU</span>:           <!--$cpu_id = SELECT cpu FROM products WHERE id=\'$i\'-->
-                                <ul>
-                                    <li><span>AMD</span> <span>Ryzen 5</span> <span>4600H</span></li> <!--SELECT cpu_brand, cpu_fam, cpu_model FROM CPU WHERE id=\'$cpu_id\'-->
-                                    <li><span>6</span> core, <span>12</span> thread @<span>3.8</span>GHz</li> <!--SELECT cpu_cores, cpu_threads, cpu_speed FROM CPU WHERE id=\'$cpu_id\'-->
-                                </ul>
-                            </div>
-                            <div class="list-group-item">
-                                <span>RAM</span>  <!--$ram_id = SELECT ram FROM products WHERE id=\'$i\'-->
-                                <ul>
-                                    <li>Quantità: <span>16</span>GB</li>  <!--SELECT ram_size FROM RAM WHERE id=\'$ram_id\'-->
-                                    <li>Tipo:<span>DDR4</span> @<span>2666</span>MHz</li>   <!--SELECT ram_type, ram_speed FROM RAM WHERE id=\'$ram_id\'-->
-                                </ul>
-                            </div>
-                            <div class="list-group-item">
-                                <span>Disco</span>: 
-                                <ul>
-                                    <li><span>SSD</span> <span>NVMe</span></li>   <!--$disk_id = SELECT disk FROM products WHERE id=\'$i\' SELECT disk_type FROM RAM WHERE id=\'$disk_id\'--> 
-                                    <li>Dimensione: <span>512</span>GB</li>   <!--SELECT disk_size FROM DISK WHERE id=\'$disk_id\'-->
-                                </ul>
-                            </div>
-                        </h6>
-                        </div>
-                        </div>';
-                  }
-                ?>
-                </div>
               </div>
           </div>
         </div>
